92fb955240228c9dfd058eae90c2257912ca107f
forhannoor <mac_bd@yahoo.com>
2023-03-30 23:53:47 -0400

    refactored DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,22 +1,21 @@
-// Implementation of DFS (Depth First Search).
-
+// Implementation of DFS(Depth First Search) algorithm.
 #include <cstdio>
-#include <list>>
+#include <list>
 
 class Graph
 {
     private:
         int _v;
-        std::list<int> *_adj;
+        std::list<int>* _adj;
         std::list<int>::iterator it;
 
     public:
         Graph(int v);
-        void addEdge(int source, int destination);
-        void removeEdge(int source, int destination);
-        void printAdjacencyMatrix();
-        void DFS(int v);
-        void ProcessDFS(int v, bool visited []);
+        void add_edge(int source, int destination);
+        void remove_edge(int source, int destination);
+        void print_adjacency_matrix();
+        void dfs(int v);
+        void process_dfs(int v, bool visited []);
 };
 
 Graph::Graph(int v)
@@ -25,15 +24,16 @@ Graph::Graph(int v)
     _adj = new std::list<int>[v];
 }
 
-void Graph::addEdge(int source, int destination)
+// Adds an edge/connection from source to destination.
+void Graph::add_edge(int source, int destination)
 {
-    if(source < _v && source >= 0 && destination >= 0) // both source and destination are valid
-    {
+    // If both source and destination are valid.
+    if(source < _v && source >= 0 && destination >= 0)
         _adj[source].push_back(destination);
-    }
 }
 
-void Graph::removeEdge(int source, int destination)
+// Removes the edge/connection from source to destination.
+void Graph::remove_edge(int source, int destination)
 {
     if(source < _v && source >= 0 && destination >= 0)
     {
@@ -48,56 +48,54 @@ void Graph::removeEdge(int source, int destination)
     }
 }
 
-void Graph::printAdjacencyMatrix()
+// Prints adjacency matrix.
+void Graph::print_adjacency_matrix()
 {
     for(int i = 0; i < _v; ++i)
     {
-        if(_adj[i].size() == 0){ continue; }
+        // If list for current vertex is empty.
+        if(_adj[i].size() == 0)
+            continue;
 
         printf("[%d] => ", i);
 
         for(it = _adj[i].begin(); it != _adj[i].end(); ++it)
-        {
             printf("%d\t", *it);
-        }
 
         printf("\n");
     }
 }
 
-void Graph::DFS(int v)
+void Graph::dfs(int v)
 {
-    bool *visited = new bool [v];
+    bool* visited = new bool [v];
 
-    for(int i = 0; i < v; ++i){ visited[i] = false; }
+    for(int i = 0; i < v; ++i)
+        visited[i] = false;
 
-    ProcessDFS(v, visited);
+    process_dfs(v, visited);
 }
 
-void Graph::ProcessDFS(int v, bool visited [])
+void Graph::process_dfs(int v, bool visited [])
 {
     visited[v] = true;
     printf("%d ", v);
 
     for(it = _adj[v].begin(); it != _adj[v].end(); ++it)
-    {
         if(! visited[*it])
-        {
-            ProcessDFS(*it, visited);
-        }
-    }
+            process_dfs(*it, visited);
 }
 
 int main()
 {
-    Graph *g = new Graph(5);
-    g->addEdge(0, 1);
-    g->addEdge(0, 2);
-    g->addEdge(1, 2);
-    g->addEdge(2, 0);
-    g->addEdge(2, 3);
-    g->addEdge(3, 3);
-    g->printAdjacencyMatrix();
-    g->DFS(3);
+    Graph* graph = new Graph(5);
+    graph->add_edge(0, 1);
+    graph->add_edge(0, 2);
+    graph->add_edge(1, 2);
+    graph->add_edge(2, 0);
+    graph->add_edge(2, 3);
+    graph->add_edge(3, 3);
+    graph->print_adjacency_matrix();
+    graph->dfs(3);
     return 0;
 }
